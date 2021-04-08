425686413ce088140093b5e4d1614929f033006e
Nander Voortman <nanderv@gmail.com>
2021-04-08 17:05:18 +0200

    Small fixes.

--- reservations/forms.py
+++ reservations/forms.py
@@ -5,24 +5,24 @@ from django.forms import ModelForm, Select, forms, ChoiceField
 from reservations.models import Reservation, Timeslot
 
 
-CATEGORIES = (
-    ('COM', 'Combat'),
-    ('CRA', 'Crafting'),
-    ('WAR', 'Warfare'),
-)
 def today_options():
     return (
         (
-        datetime.now().date(), "Today: "+ str(datetime.now().date().isoformat())
+            datetime.now().date(), "Today: " + str(datetime.now().date().isoformat())
         ),
-        ( (datetime.now()+timedelta(days=1)).date(), "Tomorrow: "+ str((datetime.now()+timedelta(days=1)).date().isoformat())),
+        ((datetime.now() + timedelta(days=1)).date(), "Tomorrow: " + str((datetime.now() + timedelta(days=1)).date().isoformat())),
         ((datetime.now() + timedelta(days=2)).date(), "Overmorrow: " + str((datetime.now() + timedelta(days=2)).date().isoformat())),
     )
+
+
 class EditForm(ModelForm):
     date = ChoiceField(choices=today_options())
-    def __init__(self,*args,**kwargs):
-        super (EditForm,self ).__init__(*args,**kwargs) # populates the post
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.get('date').choices = today_options()
         self.fields['timeslot'].queryset = Timeslot.objects.all()
+
     class Meta:
         model = Reservation
         fields = ['name',
@@ -32,6 +32,4 @@ class EditForm(ModelForm):
         labels = {'name': 'Name',
                   'timeslot': 'Timeslot'
                   }
-        widgets={'timeslot': Select}
-
-
+        widgets = {'timeslot': Select}
